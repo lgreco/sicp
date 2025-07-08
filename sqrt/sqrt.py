34eb71ee6a5c401e2800a6f483318d96538bf211
Leo Irakliotis <lgreco@gmail.com>
2025-07-08 14:05:36 +0000

    devcontainer removed

--- sqrt/sqrt.py
+++ sqrt/sqrt.py
@@ -1,10 +1,13 @@
+def average(a: float, b: float) -> float:
+    return (a + b) / 2
+
+def square(x:float)-> float:
+    return x*x
+
 def sqrt(x: float) -> float:
 
     def is_good_enough(guess: float) -> bool:
-        return abs(guess * guess - x) < 0.001
-
-    def average(a: float, b: float) -> float:
-        return (a + b) / 2
+        return abs(square(guess) - x) < 0.001
 
     def improve(guess: float) -> float:
         return average(guess, x / guess)
@@ -16,7 +19,6 @@ def sqrt(x: float) -> float:
             return sqrt_iter(improve(guess))
 
     return sqrt_iter(1.0)
-
 
 if __name__ == "__main__":
     print(sqrt(1234))
